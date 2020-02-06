5333f2d42a8ab9cd7b679b1dd0d2d730605f8a04
Adeleke10 <Adeleke10@DESKTOP-H17P76I.mshome.net>
2020-02-06 14:49:01 +0100

    First Commit

---
+++ Documents/NetBeansProjectsesigns/MavenTest/src/main/java/com/lakekudz/maventest/Junit.java
@@ -0,0 +1,19 @@
+package com.lakekudz.maventest;
+
+public class Junit {
+    public int add(int a, int b){
+        a+=b;
+        return a;
+    }
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Junit ju = new Junit();
+        System.out.println(ju.add(10, 40));
+    }
+    
+}
