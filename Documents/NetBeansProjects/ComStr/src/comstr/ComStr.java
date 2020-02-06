8c14c2fab4d2b12eb5017a777c095c7e4ff60d69
Adeleke10 <Adeleke10@DESKTOP-H17P76I.mshome.net>
2020-02-06 14:47:04 +0100

    First Commit

---
+++ Documents/NetBeansProjects/ComStr/src/comstr/ComStr.java
@@ -0,0 +1,17 @@
+
+package comstr;
+
+public class ComStr {
+    
+    public void input(){
+        
+    }
+    
+    public String combine(String a, String b)
+    {       
+        String result;
+        result=a+b;
+        return result;
+    }
+  
+   }
